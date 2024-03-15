7ff9cb708d71a91a30b8bd1101ac8128e7ed0df0
Ramiro Uziel <ramirouziel15@gmail.com>
2024-03-14 18:31:10 -0600

    Refactor app.html and remove profile page

---
+++ src/components/Profile/Sidebar.svelte
@@ -0,0 +1,18 @@
+<script>
+  let tabs = [
+    { name: "Mi Perfil", icon: "fa-solid fa-user", link: "#foo" },
+    { name: "E-wallet", icon: "fa-solid fa-wallet", link: "#foo" },
+    { name: "Compras", icon: "fa-solid fa-shopping-bag", link: "#foo" },
+  ];
+</script>
+
+<div class="text-sm flex flex-col gap-2">
+  {#each tabs as tab}
+    <a
+      href={tab.link}
+      class={"border rounded p-4 shadow-md hover:bg-st-blue-light hover:text-st-blue duration-100 "}
+    >
+      <p class="text-sm font-bold">{tab.name}</p>
+    </a>
+  {/each}
+</div>
---
+++ src/routes/cuenta/perfil/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+  import Sidebar from "/src/components/Profile/Sidebar.svelte";
+</script>
+
+<div class="p-4 grid grid-cols-3 gap-4">
+  <Sidebar class="col-span-1" />
+  <div>
+    <h1 class="text-3xl font-semibold">Mi perfil</h1>
+  </div>
+</div>
