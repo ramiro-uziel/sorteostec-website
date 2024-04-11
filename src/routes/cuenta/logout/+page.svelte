8163dd09cc88dd7d7454a8a072dbdec0cd9717ad
Ramiro Uziel <ramirouziel15@gmail.com>
2024-04-10 18:58:52 -0600

    Probando logout

--- src/routes/cuenta/logout/+page.svelte
+++ src/routes/cuenta/logout/+page.svelte
@@ -1,11 +1,25 @@
 <script>
   import { get } from "svelte/store";
   import { userProfile } from "$lib/stores";
+  import { goto } from "$app/navigation";
 
   let email;
   const user = get(userProfile);
   if (user) {
     email = user.email;
+  }
+
+  function handleLogout() {
+    document.cookie.split(";").forEach((cookie) => {
+      document.cookie = cookie
+        .replace(/^ +/, "")
+        .replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/");
+    });
+    goto("/");
+  }
+
+  function handleCancel() {
+    goto("/");
   }
 </script>
 
@@ -18,7 +32,7 @@
       <h1 class="p-3 text-3xl md:text-4xl font-semibold text-st-blue">
         Log out
       </h1>
-      <p class="p-3">¿Deseas salir de la sessión?</p>
+      <p class="p-3">¿Deseas salir de la sesión?</p>
       <div class="px-3">
         <div class="relative flex flex-row mb-2">
           <div
@@ -29,13 +43,15 @@
         </div>
         <div class="relative flex flex-row gap-2 py-2 mb-5">
           <button
-            type="submit"
+            type="button"
+            on:click={handleLogout}
             class="w-full bg-st-blue rounded p-4 text-white hover:bg-st-blue-light hover:text-st-blue duration-100"
           >
             Si
           </button>
           <button
-            type="submit"
+            type="button"
+            on:click={handleCancel}
             class="w-full bg-st-blue rounded p-4 text-white hover:bg-st-blue-light hover:text-st-blue duration-100"
           >
             No
