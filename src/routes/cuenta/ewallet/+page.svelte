03d694963179092810ebd51a0bec2ec737321c01
Ramiro Uziel <ramirouziel15@gmail.com>
2024-04-11 23:55:52 -0600

    Ewallet boton para desactivar saldo box

--- src/routes/cuenta/ewallet/+page.svelte
+++ src/routes/cuenta/ewallet/+page.svelte
@@ -169,6 +169,12 @@
   >
     <div class="fixed inset-0 bg-black opacity-50"></div>
     <div class="relative bg-white rounded-lg p-8 sm:w-[500px] xs:w-[250px]">
+      <div class="flex flex-row gap-4 p-1 pt-3 pb-3 text-st-blue">
+        <button on:click={toggleSaldoBox}>
+          <i class="fa fa-angle-left" aria-hidden="true"></i>
+        </button>
+      </div>
+
       <div class="flex flex-col justify-between h-full">
         <!-- <form name="registro"> -->
         <div>
