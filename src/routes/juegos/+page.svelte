1e75536a1c9a64e1f506e5d76c536feca0fb90a5
Ramiro Uziel <ramirouziel15@gmail.com>
2024-04-12 00:51:28 -0600

    Ruta /juegos

--- src/routes/juegos/+page.svelte
+++ src/routes/juegos/+page.svelte
@@ -4,52 +4,57 @@
   let juegos = [
     {
       color: "#7c44b7",
-      image: "/images/fof-granja.png",
+      image: "/images/fof-granja.webp",
       title: "Granja",
+      link: "/juegos/granja",
+    },
+    {
+      color: "#7c44b7",
+      image: "/images/fof-gallinero.webp",
+      title: "Gallinero",
       link: "#",
     },
     {
       color: "#7c44b7",
-      image: "/images/fof-granja.png",
-      title: "Granja",
-      link: "#",
-    },
-    {
-      color: "#7c44b7",
-      image: "/images/fof-granja.png",
-      title: "Granja",
+      image: "/images/fof-repartidor.webp",
+      title: "Repartidor",
       link: "#",
     },
   ];
 </script>
 
 <body>
-  <div class="flex justify-center items-center h-screen">
-    <div class="gameBackground flex flex-col items-center w-full h-full">
-      <div class="max-w-[1250px]">
+  <div
+    class="gameBackground flex justify-center items-center"
+    style="min-height: calc(100vh - var(--header-height));"
+  >
+    <div class=" flex flex-col items-center w-full p-10">
+      <div class="max-w-[1250px] pb-20">
         <div class="flex flex-col items-center text-white">
-          <h1 class="text-4xl font-bold">Juegos</h1>
+          <h1 class="text-4xl font-bold py-10">Juegos</h1>
+          <p class="text-3xl font-bold">Cultiva Éxitos, Cosecha Recompensas.</p>
+          <p class="text-2xl font-semibold">
+            ¡Descubre el boleto dorado y gana premios exclusivos!
+          </p>
           <div class="w-full flex justify-center p-10">
-            <div
-              class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 w-full"
-            >
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
               {#each juegos as { color, image, title, link }}
                 <div
-                  class="flex flex-col bg-white rounded-2xl p-5 hover:scale-[105%] duration-200 max-w-96"
+                  class="flex flex-col bg-white rounded-2xl p-5 hover:scale-[105%] duration-200 max-w-[320px]"
                 >
-                  <img
-                    src={image}
-                    alt={title}
-                    class="rounded-2xl duration-200"
-                  />
                   <a
                     href={link}
-                    class="bg-green-600 flex items-center justify-center mt-4 pt-3 pb-3 pl-5 pr-5 rounded-lg cursor-pointer text-white"
+                    class="bg-green-600 flex items-center justify-center mb-4 pt-3 pb-3 pl-5 pr-5 rounded-lg cursor-pointer text-white"
                     style="height: 100%;"
                   >
                     <i class="fa-solid fa-play text-xl px-1"></i>
                     <p class="text-xl font-bold px-1">{title}</p>
                   </a>
+                  <img
+                    src={image}
+                    alt={title}
+                    class="rounded-2xl duration-200"
+                  />
                 </div>
               {/each}
             </div>
---
+++ src/routes/juegos/granja/+page.svelte
@@ -0,0 +1,52 @@
+<script>
+  function goFullscreen() {
+    const elem = document.getElementById("embededGame");
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) {
+      elem.webkitRequestFullscreen();
+    }
+  }
+</script>
+
+<main
+  class="gameBackground flex justify-center items-center"
+  style="min-height: calc(100vh - var(--header-height)); "
+>
+  <div
+    class="w-full text-xs text-white flex flex-col gap-4 items-center"
+    style="max-width: 1200px; min-width: 550px; aspect-ratio: 16 / 9;"
+  >
+    <iframe
+      title="Fields of Fortune"
+      id="embededGame"
+      src="/unity/index.html"
+      class="w-full border-none"
+      style="aspect-ratio: 16 / 9;"
+    ></iframe>
+    <div class="flex flex-row gap-5">
+      <a
+        href="/juegos"
+        class="mt-4 px-4 py-2 font-bold text-base bg-st-blue hover:bg-st-blue-light text-white hover:text-st-blue rounded duration-200"
+      >
+        Ir a Juegos
+      </a>
+      <button
+        on:click={goFullscreen}
+        class="mt-4 px-4 py-2 font-bold text-base bg-st-blue hover:bg-st-blue-light text-white hover:text-st-blue rounded duration-200"
+      >
+        Maximizar
+      </button>
+    </div>
+  </div>
+</main>
+
+<style>
+  .gameBackground {
+    background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+      url("/images/fof-background.png");
+    background-size: cover; /* Ensures the image covers the entire element */
+    background-repeat: no-repeat; /* Prevents image from repeating */
+    background-position: center; /* Centers the background image */
+  }
+</style>
