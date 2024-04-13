d813be978093a45b1f77352da5b703c8d259756f
Ramiro Uziel <ramirouziel15@gmail.com>
2024-04-12 20:54:11 -0600

    Pagina juegos

--- src/routes/juegos/+page.svelte
+++ src/routes/juegos/+page.svelte
@@ -29,7 +29,7 @@
             ¡Descubre el boleto dorado y gana premios exclusivos!
           </p>
           <div class="w-full flex justify-center p-10">
-            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7">
+            <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-7">
               {#each juegos as { color, image, title, link }}
                 <div
                   class="flex flex-col bg-white rounded-2xl p-5 hover:scale-[105%] duration-200 max-w-[320px]"
