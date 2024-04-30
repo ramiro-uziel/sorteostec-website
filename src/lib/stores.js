9971296108fd8f516662402abbd78ded2d29bf56
Ramiro Uziel <ramirouziel15@gmail.com>
2024-04-29 23:32:55 -0600

    Asignar usuario como admin en /admin

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,7 @@
 import { writable } from "svelte/store";
 
+// Objetos iniciales
+
 const initialProfile = {
   name: "",
   email: "",
@@ -25,17 +27,28 @@ const initialAdminInfo = {
 const build = {
   buildID: "",
 };
+
 const personalCards = { tarjetas: [] };
 const personalTickets = [];
 const personalPurchases = { compras: [] };
+
+// STORES
+
+// perfil
 export const userProfile = writable(initialProfile);
+
+// e-wallet
 export const userWallet = writable(initialWallet);
-export const userLogged = writable(false);
-export const dataLoaded = writable(false);
-export const buildInfo = writable(build);
 export const cardList = writable(personalCards);
 export const ticketList = writable(personalTickets);
 export const purchaseList = writable(personalPurchases);
 export const abonarSaldoBoxVisible = writable(false);
+
+// Admin
 export const isAdmin = writable(false);
 export const adminInfo = writable(initialAdminInfo);
+
+// Misc
+export const userLogged = writable(false);
+export const dataLoaded = writable(false);
+export const buildInfo = writable(build);
