2b6b0617f3bc863b1bb58f956f9bc349cc3951d2
Ramiro Uziel <ramirouziel15@gmail.com>
2024-05-02 18:52:35 -0600

    Pedir registro o login a usuario en juegos

--- src/routes/juegos/+page.svelte
+++ src/routes/juegos/+page.svelte
@@ -1,7 +1,8 @@
 <script>
+  import { userLogged } from "../../lib/stores";
+
   let juegos = [
     {
-      color: "#8fcda9",
       image: "/images/fof-square.webp",
       title: "Fields of Fortune",
       link: "/juegos/granja",
@@ -41,19 +42,44 @@
           >
             ¡Descubre el boleto dorado y gana premios exclusivos!
           </p>
+          {#if !$userLogged}
+            <p
+              class="text-st-blue text-2xl font-bold drop-shadow-lg bg-st-blue-light p-2 mt-3 rounded-xl"
+            >
+              Registrate o inicia seción para jugar
+            </p>
+          {/if}
           <div class="w-full flex justify-center py-10 md:p-10">
             <div class="grid grid-cols-1 gap-7">
-              {#each juegos as { color, image, title, link }}
+              {#each juegos as { image, title, link }}
                 <div
                   class="flex flex-col rounded-xl border-white border-8 duration-200 max-w-[320px] bg-white overflow-hidden hover:scale-[101%] drop-shadow-2xl"
                 >
-                  <a
-                    href={link}
-                    class="bg-[#6ac536] hover:bg-[#b6eb8b] hover:text-lime-900 flex items-center justify-center mb-2 pt-3 pb-3 pl-5 pr-5 rounded-xl cursor-pointer text-white duration-200"
-                  >
-                    <i class="fa-solid fa-play text-xl px-1"></i>
-                    <p class="text-xl font-bold px-1">{title}</p>
-                  </a>
+                  {#if $userLogged}
+                    <a
+                      href={link}
+                      class="bg-[#6ac536] hover:bg-[#b6eb8b] hover:text-lime-900 flex items-center justify-center mb-2 pt-3 pb-3 pl-5 pr-5 rounded-xl cursor-pointer text-white duration-200"
+                    >
+                      <i class="fa-solid fa-play text-xl px-1"></i>
+                      <p class="text-xl font-bold px-1">{title}</p>
+                    </a>
+                  {:else}
+                    <div class="flex flex-row gap-2">
+                      <a
+                        href="/registro"
+                        class="bg-st-blue hover:bg-st-blue-light hover:text-st-blue flex items-center justify-center mb-2 pt-3 pb-3 w-full rounded-xl cursor-pointer text-white duration-200"
+                      >
+                        <p class="text-md font-bold px-1">Registro</p>
+                      </a>
+                      <a
+                        href="/login"
+                        class="bg-st-blue hover:bg-st-blue-light hover:text-st-blue flex items-center justify-center mb-2 pt-3 pb-3 w-full rounded-xl cursor-pointer text-white duration-200"
+                      >
+                        <p class="text-md font-bold px-1">Iniciar sesion</p>
+                      </a>
+                    </div>
+                  {/if}
+
                   <div
                     class="rounded-xl border-white border-0 duration-200 bg-white overflow-hidden"
                   >
